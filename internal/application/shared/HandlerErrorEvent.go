c29bd0e63a2564ba9b7a9d8be2e388d596e4a82a
Lucas Rego <lucas.sr.rg@gmail.com>
2022-06-17 22:43:32 -0300

    test: :test_tube: improve test coverage
    #14

--- internal/application/shared/HandlerErrorEvent.go
+++ internal/application/shared/HandlerErrorEvent.go
@@ -10,7 +10,7 @@ type HandlerError struct {
 func (e *HandlerError) Run(event Event) {
 	errorText, ok := event.Data.(string)
 	if !ok {
-		log.Fatal("HandlerError: event.Data is not string")
+		panic("HandlerError: event.Data is not string")
 	}
-	log.Fatalf("Error %s, ", errorText)
+	log.Panicf("Error %s, ", errorText)
 }
